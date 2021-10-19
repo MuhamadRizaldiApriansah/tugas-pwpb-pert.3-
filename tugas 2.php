5069498f1307441eb4152d891784612a18816b1f
MuhamadRizaldiApriansah <87842100+MuhamadRizaldiApriansah@users.noreply.github.com>
2021-10-19 20:08:36 +0700

    Add files via upload

---
+++ tugas 2.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 3 (tugas2) Muhamad Rizaldi Apriansah</title>
+</head>
+<body bgcolor="cornsilk">
+    <h1 align="center">Pertemuan ke 3</h1>
+    <h2 align="center">Tugas 2</h2>
+    <p align="center">Muhamad Rizaldi Apriansah</p>
+    <center>Pemrograman Web dan Perangkat Bergerak</center>
+    <a href="https://www.instagram.com/">Instagram</a>
+    <hr>
+    <table cellspacing="0" cellpadding="3" border="1" width="200px" height="100px" align="center">
+        <tr>
+            <th>ini adalah tabel</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td rowspan="2"></td>
+        </tr>
+        <tr>
+            <td colspan="2"></td>
+        </tr>
+    </table>
+    <pre>Berikut adalah daftar gambar</pre>
+    <ol>
+        <li>Gambar Burung Elang</li>
+    </ol>
+    <ul>
+        <li>Deskripsi tentang Burung Elang</li>
+    </ul>
+    <dl>
+        <dt>Burung Elang</dt>
+        <dd>Elang merupakan salah satu dari hewan yang terdapat di seluruh Indonesia. Dalam Bahasa inggris, eagle atau elang merujuk pada burung pemangsa berukuran besar dari suku Accipitridae terutama genus Aquila.</dd>
+    </dl>
+    <b>Foto Burung Elang ydari Google</b>
+    <img src="https://assets-a1.kompasiana.com/items/album/2016/04/23/elang-1-571aa253369773180d9c76e7.jpg">
+    <font face="Georgia" size="16"></font>
+    <div style="width:200px;height=300px;"></div>
+    <hr noshade>
+    <p align="center">by:Muhamad Rizaldi Apriansah</p>
+</body>
+</html>
